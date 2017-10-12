28dba8bd688ca8fbd5005644f37133d134a28e06
manhn <manhn@DESKTOP-0B8SF0N>
2017-10-13 00:05:01 +0700

    commited by ManhNguyen

---
+++ thefirstdemo/src/manhnt/javabasic/basicexample/helloworld.java
@@ -0,0 +1,10 @@
+package manhnt.javabasic.basicexample;
+
+public class helloworld {
+	public static void main(String[] args) {
+		int a, b;
+		a = 9;
+		b = a;
+		System.out.println("a + b: " +(a+b) );
+	}
+}
